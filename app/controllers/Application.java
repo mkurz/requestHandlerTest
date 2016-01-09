f803c70788a2ca38d3ec96a9209c9ceda5b73da2
Matthias Kurz <m.kurz@irregular.at>
2016-01-10 00:08:43 +0100

    Apply annotations

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,10 +3,15 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import utils.ClassAnnotation;
+import utils.MethodAnnotation;
+
 import views.html.*;
 
+@ClassAnnotation
 public class Application extends Controller {
 
+    @MethodAnnotation
     public Result index() {
         return ok(index.render("Your new application is ready."));
     }
