cc592a1306f00465da56a08477595e57f0bebf1e
Matthias Kurz <m.kurz@irregular.at>
2016-01-09 23:52:43 +0100

    Added annotations applied on method level

---
+++ app/utils/MethodAnnotationAction.java
@@ -0,0 +1,15 @@
+package utils;
+
+import play.libs.F.Promise;
+import play.mvc.Action;
+import play.mvc.Http.Context;
+import play.mvc.Result;
+
+public class MethodAnnotationAction extends Action<MethodAnnotation> {
+	
+	@Override
+	public Promise<Result> call(final Context ctx) throws Throwable {
+		return this.delegate.call(ctx);
+	}
+
+}
